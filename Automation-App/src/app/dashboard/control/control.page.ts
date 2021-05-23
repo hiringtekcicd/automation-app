8bed66c39a4176e61183058b11d6bdbd1d0a221d
Ajay Arumugam <ajayarumugam07@gmail.com>
2021-05-23 10:21:34 -0500

    merge conflicts fixed

--- Automation-App/src/app/dashboard/control/control.page.ts
+++ Automation-App/src/app/dashboard/control/control.page.ts
@@ -62,15 +62,10 @@ export class ControlPage implements OnInit {
         this.currentDevice = this.variableManagementService.getCurrentDeviceSettings(this.currentDeviceType, this.currentDeviceIndex);
         this.changeDetector.detectChanges();
         this.settingsForm.patchValue(this.currentDevice.settings);
-<<<<<<< HEAD
         if(this.currentDevice.settings['grow_lights']['power_outlets']) {
           this.growLightArray = this.currentDevice.settings['grow_lights']['power_outlets'];
         }
         this.powerOutlets = this.currentDevice.power_outlets
-=======
-        this.growLightArray = this.currentDevice.settings['grow_lights']['power_outlets'];
-        this.powerOutlets = this.currentDevice.power_outlets;
->>>>>>> camera-stream-tom
       } else {
         let fertigationSystemCount = this.variableManagementService.fertigationSystemSettings.value.length;
         let climateControllerCount = this.variableManagementService.climateControllerSettings.value.length;
