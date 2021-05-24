13a190f14063ed78e68f78b00aa36e50912013ac
Tom Gao <zimingg@umich.edu>
2021-05-24 17:10:02 -0400

    Navbar titles work

--- Automation-App/src/app/dashboard/dashboard.page.ts
+++ Automation-App/src/app/dashboard/dashboard.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 import { VariableManagementService } from '../Services/variable-management.service';
 
 @Component({
@@ -8,13 +8,20 @@ import { VariableManagementService } from '../Services/variable-management.servi
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  title: String;
+  constructor(private router: Router, private activeRoute : ActivatedRoute) { }
 
-  constructor(private router: Router) { }
-
-  ngOnInit() { }
+  ngOnInit() { //initial name
+    let tempRoute = this.router.url.substr(11);
+    if(tempRoute.indexOf('?') !== -1){ //if has query params, cut them out
+      tempRoute = tempRoute.substr(0,tempRoute.indexOf('?'));
+    }
+    this.title = tempRoute[0].toUpperCase() + tempRoute.substr(1);
+  }
 
   navigateToTab(tabName : string){
     this.router.navigate(['/dashboard/'+tabName], { queryParamsHandling: "preserve" });
+    this.title = tabName[0].toUpperCase() + tabName.substr(1); // capitalize this
   }
 }
 
