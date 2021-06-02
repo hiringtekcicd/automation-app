b7561884c3bb43fc16b94206c12b2357bcbc0b97
Tom Gao <zimingg@umich.edu>
2021-06-01 22:28:48 -0400

    WIP EC validation

--- Automation-App/src/app/components/ec/ec.component.ts
+++ Automation-App/src/app/components/ec/ec.component.ts
@@ -3,7 +3,6 @@ import { DayNightTargetValidator } from 'src/app/validators/daynighttarget.valid
 import { TwoValCompareValidator } from 'src/app/validators/twovalcompare.validator';
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { VariableManagementService } from 'src/app/Services/variable-management.service';
 
 @Component({
   selector: 'ec',
@@ -30,21 +29,21 @@ export class EcComponent implements OnInit, OnDestroy {
       'dose_interv': this.fb.control(null, [Validators.required, Validators.min(1), Validators.max(14400)]),
       'd_n_enabled': this.fb.control(true),
       'day_tgt': this.fb.control(null, [Validators.min(0), Validators.max(10000)]),
-      'night_tgt': this.fb.control(null, [Validators.min(0), Validators.max(10000)]), // TODO conditional
+      'night_tgt': this.fb.control(null, [Validators.min(0), Validators.max(10000)]),
       'tgt': this.fb.control(null, [Validators.min(0), Validators.max(10000)]),
       'pumps': this.fb.group({
-        'pump_1': this.fb.control(null, [Validators.min(0), Validators.max(1000)]), // TODO at least one of the following
-        'pump_2': this.fb.control(null, [Validators.min(0), Validators.max(1000)]),
-        'pump_3': this.fb.control(null, [Validators.min(0), Validators.max(1000)]),
-        'pump_4': this.fb.control(null, [Validators.min(0), Validators.max(1000)]),
-        'pump_5': this.fb.control(null, [Validators.min(0), Validators.max(1000)])
+        'pump_1': this.fb.control(null, [Validators.required, Validators.min(0), Validators.max(1000)]),
+        'pump_2': this.fb.control(null, [Validators.required, Validators.min(0), Validators.max(1000)]),
+        'pump_3': this.fb.control(null, [Validators.required, Validators.min(0), Validators.max(1000)]),
+        'pump_4': this.fb.control(null, [Validators.required, Validators.min(0), Validators.max(1000)]),
+        'pump_5': this.fb.control(null, [Validators.required, Validators.min(0), Validators.max(1000)])
       })
     }, {validators: [this.dayNightTargetValidator.dayNightTarget('tgt', 'day_tgt', 'night_tgt', 'd_n_enabled')]});
 
     this.ecForm = this.fb.group({
       'monit_only': this.fb.control(false),
       'control': this.controlForm,
-      'alarm_min': this.fb.control(null, [Validators.required, Validators.min(0), Validators.max(9999)]), //TODO do compare values
+      'alarm_min': this.fb.control(null, [Validators.required, Validators.min(0), Validators.max(9999)]),
       'alarm_max': this.fb.control(null, [Validators.required, Validators.min(1), Validators.max(10000)])
     }, {validators: [this.twoValCompareValidator.twoValCompare('alarm_min', 'alarm_max'),
                      this.ecPumpValidator.ecPumpValidator('monit_only', 'control', 'pumps')]});
