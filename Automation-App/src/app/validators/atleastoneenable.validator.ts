d0b8545a82e1c089a951c416d9df3d1cfe55ae2a
Tom Gao <zimingg@umich.edu>
2021-06-13 18:55:04 -0400

    Set errors on switches for at least one enable validator

--- Automation-App/src/app/validators/atleastoneenable.validator.ts
+++ Automation-App/src/app/validators/atleastoneenable.validator.ts
@@ -6,17 +6,45 @@ import { Injectable } from '@angular/core';
 export class AtLeastOneEnableValidator{
     public atLeastOneEnable(monitoringOnly: string, abFormName: string, a: string, b: string): ValidatorFn{
         return (form: FormGroup) => {
-            let abForm = form.get(abFormName);
-            if(!abForm || abForm === null) return null;//not supposed to happen
+            let abForm = form.get(abFormName) as FormGroup;
+            if(!abForm || abForm === null) 
+            {
+                if(abForm.controls[a].hasError('incorrect')){
+                    abForm.controls[a].setErrors(null);
+                }
+                if(abForm.controls[b].hasError('incorrect')){
+                    abForm.controls[b].setErrors(null);
+                }
+                return null;//not supposed to happen
+            }
             const monitoringOnlyVal: boolean = form.get(monitoringOnly).value;
             const aVal: boolean = abForm.get(a).value;
             const bVal: boolean = abForm.get(b).value;
             
-            if(monitoringOnlyVal === null) return null; //not supposed to happen
+            if(monitoringOnlyVal === null) 
+            {
+                if(abForm.controls[a].hasError('incorrect')){
+                    abForm.controls[a].setErrors(null);
+                }
+                if(abForm.controls[b].hasError('incorrect')){
+                    abForm.controls[b].setErrors(null);
+                }
+                return null; //not supposed to happen
+            }
             if(!monitoringOnlyVal){
                 if(aVal === null || bVal === null) return null; //not supposed to happen
-                if(!aVal && !bVal) return {"noneEnabled": true};
+                if(!aVal && !bVal){
+                    abForm.controls[a].setErrors({'incorrect': true});
+                    abForm.controls[b].setErrors({'incorrect': true});
+                    return {"noneEnabled": true};
+                }
             }else{
+                if(abForm.controls[a].hasError('incorrect')){
+                    abForm.controls[a].setErrors(null);
+                }
+                if(abForm.controls[b].hasError('incorrect')){
+                    abForm.controls[b].setErrors(null);
+                }
                 return null; //is monitoringonly, it's fine
             }
         }
