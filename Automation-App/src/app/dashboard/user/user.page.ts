d3d03c416ebc5463086064f802756fccb36641ed
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2022-10-31 23:35:45 -0600

    temporary changes to user page to avoid dependancy errors, revert changes in next push

--- Automation-App/src/app/dashboard/user/user.page.ts
+++ Automation-App/src/app/dashboard/user/user.page.ts
@@ -139,117 +139,117 @@ export class UserPage implements OnInit {
 
 
 //function for changing the users password
-  changePassword(){
+//   changePassword(){
 
-    this.alertController.create({
-      subHeader: 'Please enter your username, current password and new password',
-      inputs: [
-        {
-          name: 'email',
-          placeholder: 'email'
-        },
-        {
-          name: 'currentPassword',
-          placeholder: 'current password',
-          type: 'password'
-        },
-        {
-          name: 'newPassword',
-          placeholder: 'new password',
-          type: 'password'
-        }
+//     this.alertController.create({
+//       subHeader: 'Please enter your username, current password and new password',
+//       inputs: [
+//         {
+//           name: 'email',
+//           placeholder: 'email'
+//         },
+//         {
+//           name: 'currentPassword',
+//           placeholder: 'current password',
+//           type: 'password'
+//         },
+//         {
+//           name: 'newPassword',
+//           placeholder: 'new password',
+//           type: 'password'
+//         }
 
-      ],
+//       ],
       
-       buttons: [
-        {
-          text: 'Continue',
-          handler: (alertData) => {
-            //Subscribe
-            let num = /[0-9]/;
-            let alpha = /[a-z]/;
-            let cap = /[A-Z]/;
-            let sym = /[@#$%^&*]/;
-        if(alertData.newPassword.length >= 6){
-        if(num.test(alertData.newPassword)){
-          if (alpha.test(alertData.newPassword)){
-            if (cap.test(alertData.newPassword)){
-              if(sym.test(alertData.newPassword)){
+//        buttons: [
+//         {
+//           text: 'Continue',
+//           handler: (alertData) => {
+//             //Subscribe
+//             let num = /[0-9]/;
+//             let alpha = /[a-z]/;
+//             let cap = /[A-Z]/;
+//             let sym = /[@#$%^&*]/;
+//         if(alertData.newPassword.length >= 6){
+//         if(num.test(alertData.newPassword)){
+//           if (alpha.test(alertData.newPassword)){
+//             if (cap.test(alertData.newPassword)){
+//               if(sym.test(alertData.newPassword)){
             
-            console.log(alertData.currentPassword);
-            console.log(alertData.newPassword);
-            console.log(alertData.email);
-            this.authService.resetPassword(alertData.newPassword, alertData.currentPassword, alertData.email).subscribe(
-              resData => {
-                console.log(resData);
-              }
-            )
-            this.passwordSuccess();
-            }
-            else{this.errorPopUp();}
-          }
-          else{this.errorPopUp();}
-        }
-        else{this.errorPopUp();}
-      }
-        else{this.errorPopUp();}
-        }
-        else{this.errorPopUp();}
-        },
-        },
-        {
-          text: 'Cancel',
-          handler: () => {
+//             console.log(alertData.currentPassword);
+//             console.log(alertData.newPassword);
+//             console.log(alertData.email);
+//             this.authService.resetPassword(alertData.newPassword, alertData.currentPassword, alertData.email).subscribe(
+//               resData => {
+//                 console.log(resData);
+//               }
+//             )
+//             this.passwordSuccess();
+//             }
+//             else{this.errorPopUp();}
+//           }
+//           else{this.errorPopUp();}
+//         }
+//         else{this.errorPopUp();}
+//       }
+//         else{this.errorPopUp();}
+//         }
+//         else{this.errorPopUp();}
+//         },
+//         },
+//         {
+//           text: 'Cancel',
+//           handler: () => {
           
-          },
-        },
-      ]
-    }).then(res => {
+//           },
+//         },
+//       ]
+//     }).then(res => {
 
-      res.present();
+//       res.present();
 
-    });
-  }
+//     });
+//   }
 
-//function for deleting the user account
-  deleteAccountPopUp(){
+// //function for deleting the user account
+//   deleteAccountPopUp(){
 
-    this.alertController.create({
-      header: "Delete account",
-      subHeader: 'Are you sure you want to delete your account? Once done it is irreversible.',
-       buttons: [
-        {
-          text: 'Continue',
-          handler: (alertData) => {
+//     this.alertController.create({
+//       header: "Delete account",
+//       subHeader: 'Are you sure you want to delete your account? Once done it is irreversible.',
+//        buttons: [
+//         {
+//           text: 'Continue',
+//           handler: (alertData) => {
 
       
-            this.authService.delete().subscribe(
-              resData => {
-                console.log(resData);
-              }
-            )
+//             this.authService.delete().subscribe(
+//               resData => {
+//                 console.log(resData);
+//               }
+//             )
             
-            this.deleteSuccess();
+//             this.deleteSuccess();
        
-        },
-        },
+//         },
+//         },
 
-        {
-          text: 'Cancel',
-          handler: () => {
+//         {
+//           text: 'Cancel',
+//           handler: () => {
           
-          },
-        },
+//           },
+//         },
 
 
 
-      ]
-    }).then(res => {
+//       ]
+//     }).then(res => {
 
-      res.present();
+//       res.present();
 
-    });
-  }
+//     });
+//   }
 
  
 
